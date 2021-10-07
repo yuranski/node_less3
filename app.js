a595c6d7a94bda7db129b6629a60fa7e067a09d7
yuranski <yuriibidniak@gmail.com>
2021-10-07 13:51:30 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+
+app.listen(5000, () => {
+    console.log(`App listen 5000`);
+});
